651ab2b8a604b0969c16a98f5f3526d8ed39ae10
kristijansoft <kristijan.pe99@gmail.com>
2021-11-15 03:07:07 -0500

    update db url of production

--- config/production.js
+++ config/production.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   env: 'production',
-  db: 'mongodb+srv://shoeuzi-user:1GEBFCxnKxqcvFrm@cluster0.dbpci.mongodb.net/test?retryWrites=true&w=majority',
+  db: 'mongodb+srv://shoeuzi-user:1GEBFCxnKxqcvFrm@cluster0.dbpci.mongodb.net/shoeuzi1?retryWrites=true&w=majority',
   port: process.env.PORT || 4000,
   jwtIssuer: 'shoeuzi',
 };
